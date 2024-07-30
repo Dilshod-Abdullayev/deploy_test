4e7a326fa4e4ec36e4de10912845c42bee9c7ba4
Dilshod-Abdullayev <dilshodcoder6636@gmail.com>
2024-07-30 20:58:15 +0500

    added spesific p taggg

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 export default function App() {
   return (
     <div>
+      <h1>Spesial</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis exercitationem quis doloremque voluptate harum porro facilis labore perferendis similique, ratione suscipit animi dicta commodi, sequi optio culpa id cumque in amet, quod esse laboriosam impedit. Aliquam harum autem neque beatae sit quam, eligendi similique, obcaecati reprehenderit eum animi quod provident quidem. Odit, corporis eveniet. Possimus aperiam veritatis accusantium eligendi consequatur natus, impedit sed dolorem eum officia deleniti illo ad, temporibus voluptatem. Enim iure nesciunt assumenda eum voluptatem voluptas facilis! Ipsa, aliquid exercitationem culpa fugiat sapiente dolorem nobis. Accusamus modi aperiam impedit, mollitia obcaecati natus illum nisi earum, dicta aut error!</p>
       <h1>deploy for test</h1>
       <p>Hi from React</p>
     </div>
